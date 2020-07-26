bf06834fb4a5d03945cfc704ae95cd9e9f876565
Edison Rojas <edison.rojas@zinobe.com>
2020-07-26 16:59:23 -0500

    structure initial

---
+++ sw.js
@@ -0,0 +1,20 @@
+const STATIC_CACHE = 'satic-v1';
+const DINAMIC_CACHE = 'dinamic-v1';
+const INMUTABLE_CACHE = 'inmutable-v1';
+
+const APP_SHELL = [
+    '/',
+    'index.html',
+    'css/style.css',
+    'js/app.js',
+    'img/favicon.ico'
+]
+
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(STATIC_CACHE).then(cache => {
+            cache.addAll(APP_SHELL);
+        })
+    );
+});
