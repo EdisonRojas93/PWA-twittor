e5bac22395544404611caa2b6cb685651d8538b7
Edison Rojas <edison.rojas@zinobe.com>
2020-07-26 17:39:35 -0500

    update start url

--- sw.js
+++ sw.js
@@ -18,3 +18,16 @@ self.addEventListener('install', event => {
         })
     );
 });
+
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(res => {
+
+            if (res) return res;
+
+            return fetch(event.request)
+        })
+    );
+
+});
